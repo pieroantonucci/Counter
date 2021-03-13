031dfd3b581769d4a107749313428b6482281156
pieroantonucci <piero.antonucci@gmail.com>
2021-03-13 21:17:45 +0000

    Counter terminado con funcion reset

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function SecondsCounter(props) {
+	const [seconds, setSeconds] = useState(props.seconds);
 
-//create your first component
-export function Home() {
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSeconds(seconds => seconds + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="card">
+					<i className="fas fa-stopwatch fa-2x"></i>
+				</div>
+				<div className="card">{Math.floor(seconds / 100000) % 10}</div>
+				<div className="card">{Math.floor(seconds / 10000) % 10}</div>
+				<div className="card">{Math.floor(seconds / 1000) % 10}</div>
+				<div className="card">{Math.floor(seconds / 100) % 10}</div>
+				<div className="card">{Math.floor(seconds / 10) % 10}</div>
+				<div className="card">{seconds % 10}</div>
+			</div>
+			<div className="row" id="buttons">
+				<button
+					className="btn btn-light"
+					onClick={() => setSeconds(props.seconds)}>
+					Reset
+				</button>
+			</div>
 		</div>
 	);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { SecondsCounter } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter seconds={0} />, document.querySelector("#app"));
